ff12f3e5ae8bc123d5837b9dde07176388a16177
valentina cantarelli <valentinacanterina@gmail.com>
2021-07-29 14:44:52 +0200

    aggiornato esercizio con due cifre dopo virgola

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+//  Questo richiederà un minimo di ricerca.
+
+var km = parseInt(prompt("Ciao!Quanti Km vuoi percorrere?"));
+
+var eta = parseInt(prompt("Quanti anni hai?"));
+
+var prezzoBiglietto = km * 0.21 ;
+
+var bigliettoMinorenne;  
+
+var bigliettoSenior;
+
+
+if ( eta <= 18){
+    bigliettoMinorenne = prezzoBiglietto - prezzoBiglietto * 20 / 100;
+} else if (eta >= 65){
+    bigliettoSenior = prezzoBiglietto - prezzoBiglietto *40 / 100;
+} else {
+
+}
+
+
+if (bigliettoMinorenne){
+    alert(" Ecco il prezzo del tuo biglietto con lo sconto applicato!" + bigliettoMinorenne.toFixed(2) + '€');
+} else if(bigliettoSenior){
+    alert(" Ecco il prezzo del tuo biglietto con lo sconto applicato!" + bigliettoSenior.toFixed(2) +'€')
+}else {
+    alert("Ecco il prezzo del tuo biglietto:" + prezzoBiglietto.toFixed(2) + '€')
+}
